ce20681affc970ea1b0637add2571207df0f9b28
ShradhaR25 <79851303+ShradhaR25@users.noreply.github.com>
2022-05-29 02:37:03 +0530

    change

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# test
-microsoft_testing
+# mask-alert-tf-js
---
+++ script1A.js
@@ -0,0 +1,15 @@
+let video = document.getElementById("video");
+
+const setupCamera = () => {
+	navigator.mediaDevices
+	.getUserMedia({
+		video: { width: 600, height: 400},
+		audio: false;
+
+	})
+	.then((stream) => {
+         video.srcObject = stream;
+	});
+};
+
+setupCamera();
